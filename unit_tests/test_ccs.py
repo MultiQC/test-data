839b80957dd73a4be85e5e8e056beeee01cc210f
Redmar van den Berg <RedmarvandenBerg@lumc.nl>
2020-09-08 10:19:32 +0200

    Start writing tests for generalised parser

--- unit_tests/test_ccs.py
+++ unit_tests/test_ccs.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python3
 
-import unittest
+import pytest
 import sys
 
 # This line allows the tests to run if you just naively run this script.
 # But the preferred way is to use run_tests.sh
 sys.path.insert(0,'../MultiQC')
 
-from multiqc.modules.ccs.ccs import parse_ccs_log
+from multiqc.modules.ccs.ccs import parse_PacBio_log, parse_line
 
-class Test(unittest.TestCase):
+PARSABLE_LINES  = [
+        ('', {}),
+        ('ZMWs input (A) :', {'name':'ZMWs input', 'annotation':'A'}),
+        ('ZMWs input :', {'name':'ZMWs input'})
+        #('ZMWs input          (A)  : 93', {'name':'ZMWs input', 'count': 93})
+        #('ZMWs input          (A)  : 93', {'name':'ZMWs input', 'count': 93})
+]
 
-    def test_parse_line(self):
-        with open('data/PacBio/ccs.report.txt') as fin:
-            data = parse_ccs_log(fin)
-            assert data['ZMWs input'] == 93
-
-if __name__ == '__main__':
-    unittest.main()
+@pytest.mark.parametrize(['line', 'data'], PARSABLE_LINES)
+def test_parsable_lines(line, data):
+    parsed_line = parse_line(line)
+    assert parsed_line == data
