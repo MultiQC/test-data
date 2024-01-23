62112c2b25be394d84ea89532d45e34de2d37751
Paul Hutchings <phutchings@ksmpartners.com>
2024-01-23 09:59:50 -0500

    Rename traverse_method to walk_method

--- unit_tests/test_get_filelist.py
+++ unit_tests/test_get_filelist.py
@@ -36,31 +36,31 @@ def ignore_links(request):
     config.ignore_symlinks = request.param
 
 @pytest.mark.parametrize(["ignore_links", "expected"], [(True, ["file"]), (False, ["filelink", "nested", "file"])], indirect=["ignore_links"])
-@pytest.mark.parametrize(["traverse_method"], [(report.oswalk,), (report.pathwalk,)])
-def test_symlinked_files_found(report_init, ignore_links, traverse_method, expected):
+@pytest.mark.parametrize(["walk_method"], [(report.oswalk,), (report.pathwalk,)])
+def test_symlinked_files_found(report_init, ignore_links, walk_method, expected):
     """
     Tests that symlinked files are discovered and ignored properly.
     """
-    report.get_filelist([], traverse_method=traverse_method)
+    report.get_filelist([], walk_method=walk_method)
     searchfiles_names = [f[0] for f in report.searchfiles]
     assert all(f in searchfiles_names for f in expected)
 
-@pytest.mark.parametrize(["traverse_method"], [(report.oswalk,), (report.pathwalk,)])
-def test_excluded_dirs(ignored_dirs, report_init, traverse_method):
+@pytest.mark.parametrize(["walk_method"], [(report.oswalk,), (report.pathwalk,)])
+def test_excluded_dirs(ignored_dirs, report_init, walk_method):
     """
     Tests that ignored folder names are ignored
     """
     expected_files = {"should_be_included"}
-    report.get_filelist([], traverse_method=traverse_method)
+    report.get_filelist([], walk_method=walk_method)
     filenames = {f[0] for f in report.searchfiles}
     assert filenames == expected_files
 
-@pytest.mark.parametrize(["traverse_method"], [(report.oswalk,), (report.pathwalk,)])
-def test_excluded_paths(ignored_paths, report_init, traverse_method):
+@pytest.mark.parametrize(["walk_method"], [(report.oswalk,), (report.pathwalk,)])
+def test_excluded_paths(ignored_paths, report_init, walk_method):
     """
     Tests that ignored *folder* paths are ignored
     """
     expected_files = {"should_be_included"}
-    report.get_filelist([], traverse_method=traverse_method)
+    report.get_filelist([], walk_method=walk_method)
     filenames = {f[0] for f in report.searchfiles}
     assert filenames == expected_files
